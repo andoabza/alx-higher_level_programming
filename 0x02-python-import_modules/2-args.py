c951ebcc2a3032ed9dfe5d08c2584eb5b47bda67
andoabza <andaabi3@gmail.com>
2023-06-08 05:15:07 -0700

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
-lent = len(sys.argv)-1
-char = sys.argv[1: lent]
-if lent == 0:
-    print("{:d} arguments.".format(lent))
-elif lent == 1:
-    print("{:d} argument :".format(lent))
-else:
-    print("{:d} arguments :".format(lent))
-i = 0
-for i in range(lent + 1):
-    print("{}: {}".format(i + 1, str(sys.argv[i])))
-    i = i + 1
-print(char)
+if __name__ == "__main__":
+    lent = len(sys.argv)-1
+    if lent == 0:
+        print("{:d} arguments.".format(lent))
+    elif lent == 1:
+        print("{:d} argument :".format(lent))
+    else:
+        print("{:d} arguments :".format(lent))
+    i = 0
+    for i in range(lent + 1):
+        print("{}: {}".format(i + 1, str(sys.argv.__getitem__(i))))
+        i = i + 1
+            
