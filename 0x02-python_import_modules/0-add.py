19b91a78bdfbdf5fb36ec9c577f449952d6f16ee
andoabza <andaabi3@gmail.com>
2023-06-08 02:52:23 -0700

    add

--- 0x02-python_import_modules/0-add.py
+++ 0x02-python_import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
-sum = add_0.add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, sum))
+print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python_import_modules/2-args.py
+++ 0x02-python_import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 lent = len(sys.argv)-1
+char = sys.argv[1: lent]
 if lent == 0:
     print("{:d} arguments.".format(lent))
 elif lent == 1:
@@ -11,3 +12,4 @@ i = 0
 for i in range(lent + 1):
     print("{}: {}".format(i + 1, str(sys.argv[i])))
     i = i + 1
+print(char)
