4017ff8a20abde41065e7873c208ecbec5868095
andoabza <andaabi3@gmail.com>
2023-06-08 01:48:09 -0700

    args

---
+++ 0x02-python_import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+lent = len(sys.argv)-1
+if lent == 0:
+    print("{:d} arguments.".format(lent))
+elif lent == 1:
+    print("{:d} argument :".format(lent))
+else:
+    print("{:d} arguments :".format(lent))
+i = 0
+for i in range(lent + 1):
+    print("{}: {}".format(i + 1, str(sys.argv[i])))
+    i = i + 1
