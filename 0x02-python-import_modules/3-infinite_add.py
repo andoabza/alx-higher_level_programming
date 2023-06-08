5260cf6d6f48103a76bd2b88b1d985ab22ef12da
andoabza <andaabi3@gmail.com>
2023-06-08 05:47:26 -0700

    add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main_":
+    import sys
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+    print("{}".format(sum))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
