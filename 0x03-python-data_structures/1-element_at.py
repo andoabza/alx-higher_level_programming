334c9ecb4636a58faea0c438d8b0be011741b4d8
andoabza <andaabi3@gmail.com>
2023-06-08 23:54:03 -0700

    list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx == -idx and idx > len(my_list):
+        return None
+    else:
+        for i in range(len(my_list)):
+            print(i)
+            
