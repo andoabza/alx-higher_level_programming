2ff44c9675015781fa2629cee3e130ce1dd30cfb
andoabza <andaabi3@gmail.com>
2023-06-10 07:16:08 -0700

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-     for i in matrix:
-          matrix = ' '.join(map(str, i))
-          print("{}".format(matrix))
+    if matrix is None:
+        print("")
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != len(matrix[i]) - 1:
+                    print(" ", end="")
+            print("")
