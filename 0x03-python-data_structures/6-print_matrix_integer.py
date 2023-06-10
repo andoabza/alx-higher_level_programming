3cb320bb716c48c6fbfb851550bd923026ea40fe
andoabza <andaabi3@gmail.com>
2023-06-10 07:07:46 -0700

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+     matrix = '\n'.join([''.join(['{:3}'.format(item) for item in row]) for row in matrix])
+     print("{}".format(matrix), end=" ")
