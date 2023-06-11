133d0dee9f74ba2a2c9b77d517d8f1ec0b0c7b40
andoabza <andaabi3@gmail.com>
2023-06-11 09:29:37 -0700

    pali

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,3 +20,4 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+#endif
