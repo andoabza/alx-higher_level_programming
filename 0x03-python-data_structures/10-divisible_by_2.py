b7d05181f7b1138f850d4ee5b0f8e64d59ec8ef3
andoabza <andaabi3@gmail.com>
2023-06-11 08:58:11 -0700

    delete

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = []
+    for i in enumerate(my_list):
+        if i / 2 == True:
+            new = my_list[i]
+            return my_list
