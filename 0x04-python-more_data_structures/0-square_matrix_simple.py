ef864d68da04fb24d49bbac7687c17595916a7d0
andoabza <terbu102@gmail.com>
2023-06-14 12:58:09 -0700

    replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    newlist = []
-    for i in range(len(matrix)):
-        newlist =  [j * j for j in range(len(matrix[i]))]
-        def sqare(j):
-            return x*x
-        newlist = map(sqare, matrix)
-        return matrix 
+
+        
