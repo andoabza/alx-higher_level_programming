36c7e524d99d109a2bfd4d143a2f42e07600a326
andoabza <terbu102@gmail.com>
2023-06-15 11:06:58 -0700

    dict

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    a_dick = {}
+    a_dick.update((key, value * 2) for key, value in a_dictionary.items())
+    return a_dick
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
