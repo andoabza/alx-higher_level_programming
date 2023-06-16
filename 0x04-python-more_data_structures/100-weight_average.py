7fb86c341220711a44f1e2b17682704bf283bed5
andoabza <terbu102@gmail.com>
2023-06-16 04:01:31 -0700

    average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == 0:
+        return 0
+    else:
+        for g in range(len(rate)):
+        rate[g] = rate[g] * (amount[g] / sum(amount))
+        rate = sum(rate)
