95fc07c86d321f3c80a51afc044073d00e359427
ando <terbu102@gmail.com>
2023-06-27 12:45:03 -0700

    attr

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,8 +3,8 @@ class Square:
     """ define square class that raise exception if false"""
     def __init__(self, size=0):
         """ self private attribute"""
-        self.__size = size
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
