44beb84cc9967d009c44e20cc860662b5982ebb6
ando <terbu102@gmail.com>
2023-06-26 23:56:28 -0700

    is int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-            return int(value)
+        if value == int(value):
+            value = True
+            return value
     except:
         return False
