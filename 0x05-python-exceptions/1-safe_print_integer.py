0b04ce9deeb45c2b97bcd9970a7a0436e4e9966d
ando <terbu102@gmail.com>
2023-06-27 00:06:46 -0700

    is int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value is int(value):
+        if value == int(value):
             print("{:d}".format(value))
             return True
     except:
