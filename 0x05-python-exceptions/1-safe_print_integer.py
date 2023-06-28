ad2603930f384163817b89cb2d9720b9e1c0fae7
ando <terbu102@gmail.com>
2023-06-28 11:56:09 -0700

    int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-        if value == int(value):
-            try:
-                print("{:d}".format(value))
-                return True
-        except:
-            return False
+    try:
+        if value is int(value):
+            print("{:d}".format(value))
+            return True 
+    except:
+        return False
