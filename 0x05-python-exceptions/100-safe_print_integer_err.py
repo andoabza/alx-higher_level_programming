034989b5e9f3af7b953c1d7be173cc7844ae5768
ando <terbu102@gmail.com>
2023-06-28 14:06:33 -0700

    stder

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as e:
-        def eprint(*args, **kwargs):
-            print(*args, file=sys.stderr, **kwargs)
-        eprint(e)
+       print("Exception: {}".format(e), file=sys.stderr)
 
