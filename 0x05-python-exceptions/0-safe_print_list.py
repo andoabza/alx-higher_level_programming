4246a7820b7e7bf64d18a0be783a33f04567d352
ando <terbu102@gmail.com>
2023-06-28 11:20:40 -0700

    try except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try
+    try:
+        ret = 0
+        for i in range(0, x):
+            ret +=1
+    except:
+            return ret
