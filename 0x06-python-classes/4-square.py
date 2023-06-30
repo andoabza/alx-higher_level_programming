fdd8245037fb3fbc9657f8e0c948bd6d5cb005dc
ando <terbu102@gmail.com>
2023-06-29 22:39:29 -0700

    setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 """ define square class"""
 class Square:
-    """ def init tosize.
+    """func:
+            define init tosize.
+            define size with getter.
+            define size to setter of size.
+            define area with return size ** 2.
     Args:
         int (size) for the input"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.size = size
-        self.__size = size
-        def size(self):
-            return self.__size
-            def size(self, value):
-                self.__size = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self._size = value
+    
     def area(self):
-        return self.__size * self.__size
+        return self.size * self.size
