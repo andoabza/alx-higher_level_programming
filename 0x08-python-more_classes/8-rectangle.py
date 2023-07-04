0852a44a76658efda967dab033cce84221768609
ando <terbu102@gmail.com>
2023-07-04 05:58:44 -0700

    rect is bigger or equal

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -73,13 +73,16 @@ class Rectangle:
         print("Bye rectangle...")
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(dict_1, Rectangle):
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        elif not isinstance(dict_2, Rectangle):
+        elif not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        elif rect_1 == rect_2:
+        elif rect_1.area() == rect_2.area():
             return rect_1
-        elif rect_1 > rect_2:
+        if rect_1.area() >= rect_2.area():
             return rect_1
-        elif rect_2 > rect_1:
+        else:
             return rect_2
+
+
+
