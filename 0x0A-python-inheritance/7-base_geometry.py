a118cd4954b19f2105bfe67ce772ef4e7105b4a7
ando <terbu102@gmail.com>
2023-07-10 12:36:42 -0700

    no test

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" define a class that return """
+class BaseGeometry:
+    """ area a func that raise exception. """
+    def area(self):
+        raise Exception("area() is not implemented")
+    """ func that return namme"""
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
