d3ef1ba41b6d578ee42401b6dca659845aa1bca5
ando <terbu102@gmail.com>
2023-07-10 12:15:27 -0700

    never

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+""" define a function that inhert. """
 def inherits_from(a_class, obj):
+    """ return TRue and False. """
     if isinstance(obj, type(a_class)) and issubclass(type(obj), a_class):
         return False
     return True
