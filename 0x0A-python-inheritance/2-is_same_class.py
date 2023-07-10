a99f457940bf6cdb05f0ba648895b57902ca84b5
ando <terbu102@gmail.com>
2023-07-10 10:53:25 -0700

    als

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,6 +2,6 @@
 """ define a func that check if a class and return the class na,e. """ 
 def is_same_class(obj, a_class):
     """ that return True if is class of a_class else False. """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     return False
