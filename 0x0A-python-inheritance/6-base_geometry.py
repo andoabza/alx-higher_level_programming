2ad608547ec07c35ab4b40b663b682cfea3967e4
ando <terbu102@gmail.com>
2023-07-10 12:21:38 -0700

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pythono3
+""" define a class that return """
+class BaseGeometry:
+    """ area a func that raise exception. """
+    def area(self):
+        raise Exception("area() is not implemented")
