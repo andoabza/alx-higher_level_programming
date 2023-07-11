38e93d9abd1ccd6c6551b6f7a21081f30c1b94fc
ando <terbu102@gmail.com>
2023-07-11 01:22:29 -0700

    open read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" define a function that read file. """
+def read_file(filename=""):
+    """ open file name."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
