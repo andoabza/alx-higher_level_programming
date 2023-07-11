820aad3f61210d27a4695c5a092c14e851ec8ca5
ando <terbu102@gmail.com>
2023-07-11 11:49:36 -0700

    load

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,8 +4,8 @@ import json
 """ define  load_from_json_file for save."""
 def load_from_json_file(filename):
     """ oen file in read mode. """
-    with open(filename, 'r') as f:
-        return json.dumps(filename)
+    with open(filename) as f:
+        return (json.load(f))
 
 
 
