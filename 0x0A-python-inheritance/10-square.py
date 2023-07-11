240145f7fcefb534340042940386cd05dd1e2c89
ando <terbu102@gmail.com>
2023-07-10 22:49:12 -0700

    print

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+""" import  class. """
 Rectangle = __import__('9-rectangle').Rectangle
+""" define class. """
 class Square(Rectangle):
+    """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+    """
     def __init__(self, size):
+        """ define every thing. """
         super().integer_validator("size", size)
+        super().__init__(size, size)  
         self.__size = size
-    def area(self):
-        return self.__size ** 2
-    def __str__(self):
-        """ write the output. """
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__size) + "/" + str(self.__size)
-        return string
