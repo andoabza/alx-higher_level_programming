9b44a7bad8011271cb72fec8ad063c2d2062b27d
ando <terbu102@gmail.com>
2023-07-14 11:32:32 -0700

    print with x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,21 +3,25 @@
 from models.base import Base
 """ define a class. """
 class Rectangle(Base):
+
     """
     write a class that inhert fromn `Base` and a class of the folloeing args with private attribiute of width, height, x, y with getter setter.
     Args: width, height, x, y
     value to manage the exceptions.
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+    
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        self.id = id
         super().__init__(id)
+    
     @property
     def width(self):
         return self.__width
+    
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -25,9 +29,11 @@ class Rectangle(Base):
         elif value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
+    
     @property
     def height(self):
         return self.__height
+    
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -35,9 +41,11 @@ class Rectangle(Base):
         elif value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
+    
     @property
     def x(self):
         return self.__x
+    
     @x.setter
     def x(self, value):
         if not isinstance(value, int):
@@ -45,9 +53,11 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
+    
     @property
     def y(self):
         return self.__y
+    
     @y.setter
     def y(self, value):
         if not isinstance(value, int):
@@ -55,13 +65,18 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+    
     """ define area to calculate area. """
     def area(self):
         return self.__width * self.__height
+    
     """ define display to print #"""
     def display(self):
-        for i in range(0, self.__height):
-            print('#' * self.__width)
+         for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
+    
     """ define __str__ to print out string representation. """
     def __str__(self):
         return f"{self.__class__.__name__} ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
--- 0x0C-python-almost_a_circle/nain.py
+++ 0x0C-python-almost_a_circle/nain.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" 1-main """
+""" 6-main """
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 2)
-    print(r1.id)
+    r1 = Rectangle(2, 3, 2, 2)
+    r1.display()
 
-    r2 = Rectangle(2, 10)
-    print(r2.id)
+    print("---")
 
-    r3 = Rectangle(10, 2, 0, 0, 12)
-    print(r3.id)
+    r2 = Rectangle(3, 2, 1, 0)
+    r2.display()
