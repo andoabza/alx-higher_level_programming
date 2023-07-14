0019784796c6097205a0712c907541c217f62e6f
ando <terbu102@gmail.com>
2023-07-14 10:44:45 -0700

    area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ class Rectangle(Base):
     """
     write a class that inhert fromn `Base` and a class of the folloeing args with private attribiute of width, height, x, y with getter setter.
     Args: width, height, x, y
+    value to manage the exceptions.
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
@@ -18,26 +19,44 @@ class Rectangle(Base):
         return self.__width
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
     @property
     def height(self):
         return self.__height
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
     @property
     def x(self):
         return self.__x
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
     @property
     def y(self):
         return self.__y
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
-
+    """ define area to calculate area. """
+    def area(self):
+        return self.__width * self.__height
 
 
 
