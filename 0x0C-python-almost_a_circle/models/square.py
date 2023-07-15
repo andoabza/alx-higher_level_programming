4ed8b0eca0bef97939a43356a063f5103f83386c
ando <terbu102@gmail.com>
2023-07-15 10:35:00 -0700

    args kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,7 +26,37 @@ class Square(Rectangle):
     """ string representation. """
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+    def update(self, *args, **kwargs):
+        """ update the args with kwargs. """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
 
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.width = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
 
 
