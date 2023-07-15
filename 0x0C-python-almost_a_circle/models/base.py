a65d9b7b3e12fc05ebfadc91b5b8007b0843c90c
ando <terbu102@gmail.com>
2023-07-15 11:49:26 -0700

    json dumps

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" import json """
+import json
 """ define a class. """
 class Base:
     """ define a class that assing a value to id
@@ -12,4 +14,9 @@ class Base:
         if self.id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        
+    """ json string """
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
