56a311b489c69ef4a42500cce93862244efeaf6a
andoabza <terbu102@gmail.com>
2023-08-15 11:40:13 -0700

    score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
--- order by value desc whre score >+ 10
-SELECT name, score FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- order by value desc whre score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
