f158ed6362f9c388c4a763527988d8670318352b
andoabza <terbu102@gmail.com>
2023-08-16 21:54:07 -0700

    grant all

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- grant select
-GRANT SELECT ON hbtn_0d_2.* TO user_0d2'@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d2@localhost;
