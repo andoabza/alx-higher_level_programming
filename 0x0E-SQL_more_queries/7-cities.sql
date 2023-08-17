0cd55dae82f6b5d590a99aa4ad4d2f61b73784cf
andoabza <terbu102@gmail.com>
2023-08-16 23:09:53 -0700

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- craete user and foreign key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	FOREIGN KEY(state_id) REFERENCES STATES(id));
