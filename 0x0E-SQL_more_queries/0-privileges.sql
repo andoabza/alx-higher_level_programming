a06dab0d6f62d0d6619ee13c49adf0397dbc7f91
andoabza <terbu102@gmail.com>
2023-08-16 21:51:44 -0700

    nexy

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- show priviledges
 SHOW GRANTS FOR user_0d_1@localhost;
+-- show fot 2
 SHOW GRANTS FOR user_0d_2@localhost;
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- create user and database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+-- create user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grant select
+GRANT SELECT ON hbtn_0d_2.* TO user_0d2'@localhost;
