069fec4dd459dae8ef6e354d90986807e3a50e5a
anda <terbu102@gmail.com>
2023-09-10 23:20:28 -0700

    argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const process = require('process');
+var argv = process.argv;
+
+if (argv.length == 2)
+{
+	console.log("No argument");
+}
+if (argv.length == 3)
+{
+        console.log("Argument found");
+}
+if (argv.length > 3)
+{
+        console.log("Arguments found");
+}
