02b66473db2373c57fd08de4432587399029d261
anda <terbu102@gmail.com>
2023-09-11 12:09:40 -0700

    add

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,15 +2,15 @@
 const process = require('process');
 var argv = process.argv;
 
-if (argv.length == 2)
+if (argv.length === 2)
 {
 	console.log("No argument");
 }
-if (argv.length == 3)
+if (argv.length === 3)
 {
         console.log("Argument found");
 }
-if (argv.length > 3)
+else
 {
         console.log("Arguments found");
 }
