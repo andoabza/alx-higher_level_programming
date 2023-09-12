cf70ffb9c8cffa6188b305f6949f51125ef57007
anda <terbu102@gmail.com>
2023-09-11 22:37:18 -0700

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-let num = Number(process.argv[2]);
-if (num)
-{
-	console.log('My number: '.concat(num));
-}else {
-	console.log('Not a number');
+const num = Number(process.argv[2]);
+if (num) {
+  console.log('My number: '.concat(num));
+} else {
+  console.log('Not a number');
 }
-
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let i = 0;
+while (i < 1) {
+  console.log('C is fun \nPython is cool \nJavascript is amazing');
+  i++;
+}
