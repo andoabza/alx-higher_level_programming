827105dbc8b84e9229e1021bcb7a3266fe613d26
anda <terbu102@gmail.com>
2023-09-16 12:02:34 -0700

    cursr

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
+""" print all name from database """
 import MySQLdb
 import sys
 
-user_name = sys.argv[1]
-user_passwd = sys.argv[2]
-db_name = sys.argv[3]
-var = sys.argv[4]
-db = MySQLdb.connect(user=user_name, password=user_passwd, db=db_name, host='localhost', port=3306)
-cursor = db.cursor()
-query = "SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"
-cursor.execute(query, (var,))
-cities = cursor.fetchall()
-citi = list(citi[0] for citi in cities)
-print(*citi, sep=", ")
-cursor.close()
-db.close()
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], db=sys.argv[3], host='localhost', port=3306)
+    cursor = db.cursor()
+    query = ("""SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC""")
+    cursor.execute(query, (sys.argv[4],))
+    cities = cursor.fetchall()
+    citi = list(citi[0] for citi in cities)
+    print(*citi, sep=", ")
+    cursor.close()
+    db.close()
 
