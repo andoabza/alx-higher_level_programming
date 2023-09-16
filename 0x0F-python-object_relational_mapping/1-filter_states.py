3ad67ed9cd5b0ccbad00e2bf6efbb1d933c7a43d
anda <terbu102@gmail.com>
2023-09-16 11:39:19 -0700

    comm

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,11 +2,14 @@
 """ list all state based on id"""
 import MySQLdb
 import sys
+
+
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
                      db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC""")
+    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC""")
     states = cur.fetchall()
     for state in states:
         print(state)
