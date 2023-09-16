2a339f78d6bb36434600caab0519a814493427ac
anda <terbu102@gmail.com>
2023-09-16 11:32:44 -0700

    next

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,13 +2,13 @@
 import MySQLdb
 import sys
 """ list all state based on id"""
-db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
                      db=sys.argv[3], port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
-states = cur.fetchall()
-for state in states:
-    print(state)
-
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
