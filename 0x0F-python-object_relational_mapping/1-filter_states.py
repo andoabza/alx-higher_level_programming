ccc0531f0d2535d3b9ce0ccd6e441b3ead8bcb17
anda <terbu102@gmail.com>
2023-09-16 11:36:30 -0700

    next

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+""" list all state based on id"""
 import MySQLdb
 import sys
-""" list all state based on id"""
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
                      db=sys.argv[3], port=3306)
