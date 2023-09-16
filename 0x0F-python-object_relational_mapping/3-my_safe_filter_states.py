66a8c65860b0f04de75016881b557b35883390d4
anda <terbu102@gmail.com>
2023-09-16 11:53:32 -0700

    sys.argc

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,18 +2,19 @@
 """ print all name from database hbtn_0e_0_usa """
 import MySQLdb
 import sys
-if len(sys.argv[4]) < 9:
-    var = sys.argv[4]
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+if __name__ == "__main__":
+    if len(sys.argv[4]) < 9:
+        var = sys.argv[4]
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                      db=sys.argv[3], host='localhost', port=3306)
-cursor = db.cursor()
-try:
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(var)
-    cursor.execute(query)
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-except:
-    pass
-cursor.close()
-db.close()
+    cursor = db.cursor()
+    try:
+        query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(var)
+        cursor.execute(query)
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+    except:
+        pass
+    cursor.close()
+    db.close()
