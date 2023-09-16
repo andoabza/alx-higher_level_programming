a3fa2195de7edc2f4cf0a72d4161f161c5ff128c
anda <terbu102@gmail.com>
2023-09-16 12:41:36 -0700

    nevada

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a script that import stste and base class """
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+for instance in session.query(State).order_by(State.id):
+    print(instance.id, instance.name, sep=": ")
