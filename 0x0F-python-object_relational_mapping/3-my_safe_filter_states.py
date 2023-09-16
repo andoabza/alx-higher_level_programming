ab92e2f6aaefa950868d9ba500c4ef854051bdc1
anda <terbu102@gmail.com>
2023-09-16 11:52:00 -0700

    sys.argc

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+""" print all name from database hbtn_0e_0_usa """
 import MySQLdb
 import sys
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]   
 if len(sys.argv[4]) < 9:
     var = sys.argv[4]
-
-db = MySQLdb.connect(user=mysql_username, passwd=mysql_password,
-                     db=database_name, host='localhost', port=3306)
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                     db=sys.argv[3], host='localhost', port=3306)
 cursor = db.cursor()
 try:
     query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(var)
