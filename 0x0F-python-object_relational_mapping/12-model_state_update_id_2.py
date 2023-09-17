d99f3f8edfe8bfcb5ce3b44814eecc04fd7b5c19
anda <terbu102@gmail.com>
2023-09-16 22:24:47 -0700

    filter_by

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "_main__":
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new = session.query(State).filter_by(id=2).first()
+    new.name = 'New Mexico'
+    session.commit()
