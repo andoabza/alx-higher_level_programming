8a251b0202b9b21d45a7992b05a1714b33b294d6
anda <terbu102@gmail.com>
2023-09-16 22:30:22 -0700

    session

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" a script that change the name to New Mexico """
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "_main__":
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new = session.query(State).filter_by(id=2).first()
-    new.name = 'New Mexico'
+    new_instance = session.query(State).filter_by(id=2).first()
+    new_instance.name = 'New Mexico'
     session.commit()
