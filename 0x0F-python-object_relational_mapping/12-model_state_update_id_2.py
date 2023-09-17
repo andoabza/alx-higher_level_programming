53caef87494ae16ce2d65555531acafd9ac633f1
anda <terbu102@gmail.com>
2023-09-16 22:28:03 -0700

    filter_by

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,7 +3,7 @@
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 
 
 if __name__ == "_main__":
