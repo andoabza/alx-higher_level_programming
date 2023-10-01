a482b7b7cf2990bc29ab3b414f819641a4c53fd8
anda <terbu102@gmail.com>
2023-09-30 22:59:39 -0700

    param

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send key-value pair
-curl -sX POST "email: test@gmail.com, subject: I will always be here for PLD" "$1"
+curl -sX POST -d "email: test@gmail.com, subject: I will always be here for PLD" "$1"
