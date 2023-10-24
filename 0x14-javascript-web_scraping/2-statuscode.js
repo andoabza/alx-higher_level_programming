4bb7f83b853d0badcd0f67571c6dd81bf48005fb
anda <terbu102@gmail.com>
2023-10-24 06:01:16 -0700

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/** a script that print status code **/
+const req = require('request');
+req
+  .get(process.argv[2])
+  .on('response', function (response) {
+    console.log(response.statusCode);
+  });
