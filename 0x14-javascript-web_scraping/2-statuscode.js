2083250eed6367375078c2a2e0f146b202a371c8
anda <terbu102@gmail.com>
2023-10-24 06:12:24 -0700

    cury brace

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 /** a script that print status code **/
 const req = require('request');
+
 req
   .get(process.argv[2])
-  .on('response', function (response) {
-    console.log(response.statusCode);
+  .on('response', function (response){
+
+  console.log("code: ", response.statusCode);
   });
