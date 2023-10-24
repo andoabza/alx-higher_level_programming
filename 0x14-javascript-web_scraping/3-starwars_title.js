85a40a2143deebf5256f2d8acdb430e6de041efb
anda <terbu102@gmail.com>
2023-10-24 07:54:50 -0700

    title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/** a script that return title of movie id **/
+const req = require('request');
+const uri = 'https://swapi-api.alx-tools.com/api/films/';
+
+const url = uri.concat('', process.argv[2]);
+req.get({
+  uri: url,
+  encoding: 'utf-8',
+  gzip: true
+},
+function (err, resp, body) {
+  if (err) throw(err);
+  body = JSON.parse(body);
+  console.log(body.title);
+}
+);
